f79cda4870182ee8c295b56735855e679d8daae4
L.Prashanth <112462638+lankipalliprasanth@users.noreply.github.com>
2022-09-26 14:30:45 +0530

    Create ReverseString.java

---
+++ ReverseString.java
@@ -0,0 +1,9 @@
+public class ReverseString {
+    public static void main(String[] args) {
+        String s1 = "Hello World";
+        for(int i=s1.length()-1;i>=0;i--)
+            {
+                System.out.print(s1.charAt(i));
+            }
+    }
+}
