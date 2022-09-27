635080694ed9d08215cedd58d608f76b75edf9f5
L.Prashanth <112462638+lankipalliprasanth@users.noreply.github.com>
2022-09-27 14:07:46 +0530

    Create class years.java

---
+++ class years.java
@@ -0,0 +1,9 @@
+public class years
+ {  
+   public static void main (String args[])  
+    {   int days, years;  
+                         days = 799;  
+                         years = days / 365;  
+                         System.out.println("Number of years is:"+years);  
+    }
+}  
